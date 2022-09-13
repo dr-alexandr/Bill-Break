15b5e12cf28f3ae4d5c9bd1a4f8b5b6ba34900cd
dr-alexandr <makss03052000@gmail.com>
2022-09-13 21:00:05 +0300

    Initial Commit

---
+++ Bill Break/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bill Break
+//
+//  Created by Dr.Alexandr on 13.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
